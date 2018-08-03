79a111443b0afc1834560cd2c006d8a5ab2c37c2
Courtney Adams <cadams8238@gmail.com>
2018-08-03 09:09:50 -0700

    added cat and dog objects:

--- index.js
+++ index.js
@@ -14,25 +14,78 @@ const dog = [
   {
     imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
     imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-    name: 'Zeus',
-    sex: 'Male',
-    age: 3,
+    name: 'Goldie',
+    sex: 'Female',
+    age: 7,
     breed: 'Golden Retriever',
     story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://images.pexels.com/photos/247937/pexels-photo-247937.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350',
+    imageDescription: 'A happy husky puppy sitting outside',
+    name: 'Curtis',
+    sex: 'Male',
+    age: 2,
+    breed: 'Husky',
+    story: 'Abandoned'
+  },
+  {
+    imageURL: 'https://images.pexels.com/photos/879788/pexels-photo-879788.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350',
+    imageDescription: 'A happy beagle with a treat.',
+    name: 'Mingus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Beagle',
+    story: 'Owner moved away'
+  },
+  {
+    imageURL: 'https://images.pexels.com/photos/776078/pexels-photo-776078.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350',
+    imageDescription: 'Frenchie puppy chewing on a stick',
+    name: 'Blaze',
+    sex: 'Male',
+    age: 1,
+    breed: 'French Bulldog',
+    story: 'Abandoned'
   }
 ];
 
 const cat = [
   {
-    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageURL:'https://images.pexels.com/photos/326875/pexels-photo-326875.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350',
+    imageDescription: 'Siamese cat sitting on porch table.',
+    name: 'Lady',
+    sex: 'Female',
+    age: 5,
+    breed: 'Siamese',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
     imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
     name: 'Fluffy',
     sex: 'Female',
     age: 2,
     breed: 'Bengal',
-    story: 'Thrown on the street'
+    story: 'Owner moved away'
+  },
+  {
+    imageURL:'https://images.pexels.com/photos/17773/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=350',
+    imageDescription: 'Bashful Calico kitten hanging out outside.',
+    name: 'Spot',
+    sex: 'Female',
+    age: <1,
+    breed: 'Calico',
+    story: 'Abandoned'
+  },
+  {
+    imageURL:'https://images.pexels.com/photos/751050/pexels-photo-751050.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350',
+    imageDescription: 'Fluffy orange tabby sitting on a couch.',
+    name: 'Sebastian',
+    sex: 'Male',
+    age: 15,
+    breed: 'Tabby',
+    story: 'Terminally ill'
   }
-
 ];
 
 app.use(
