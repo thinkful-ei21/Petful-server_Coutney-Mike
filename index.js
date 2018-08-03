12eefe67668824d0a3689cbb57405a3bfbb2d362
Courtney Adams <cadams8238@gmail.com>
2018-08-03 08:44:32 -0700

    preparing for merge

--- index.js
+++ index.js
@@ -29,27 +29,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/cat', (req, res) => {
-  res.json({
-  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-  name: 'Fluffy',
-  sex: 'Female',
-  age: 2,
-  breed: 'Bengal',
-  story: 'Thrown on the street'
-});
+  res.json(cat);
 })
 
 app.get('/api/dog', (req, res) => {
-  res.json({
-  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-  name: 'Zeus',
-  sex: 'Male',
-  age: 3,
-  breed: 'Golden Retriever',
-  story: 'Owner Passed away'
-});
+  res.json(dog);
 })
 
 function runServer(port = PORT) {
